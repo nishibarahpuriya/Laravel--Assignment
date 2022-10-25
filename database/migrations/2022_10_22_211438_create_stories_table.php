620468e777638f3f643a33aa68ff5971740cd43a
nishibarahpuriya <75935025+nishibarahpuriya@users.noreply.github.com>
2022-10-25 23:14:45 +0530

    Add files via upload
    files upload

---
+++ database/migrations/2022_10_22_211438_create_stories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->dateTime('publish_date');
+            $table->enum('status', ['Active', 'Inactive','Archived'])->default('Inactive');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stories');
+    }
+};
